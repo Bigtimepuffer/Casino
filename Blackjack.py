f0f2c8c79a23abdf6c507b8c011c02f1c7a439a2
SwaggestPenguin <84942806+SwaggestPenguin@users.noreply.github.com>
2021-05-31 10:33:17 +1200

    Updated end of version 1
    Added comments and made better.

--- Blackjack.py
+++ Blackjack.py
@@ -1,11 +1,21 @@
+#Filename: Blackjack.py
+#Author: Campbell Bain
+#Date: 31/05/2021
+#Description: This is a game of blackjack where you try and gain as much money as possible without going bankrupt.
+
+
+
 import random
 import time
 import json
 
+#Setting up a save file for the money, losses, wins, bankrupts and the amount of debt you have.
 json_attack = json.load(open('jason_derulo.json', 'r'))
 money = json_attack['Money']
 losses = json_attack['Losses']
 wins = json_attack['Wins']
+bankrupts = json_attack['Bankrupts']
+debt = json_attack['Debt']
 
 ace = 11
 card_list = [ace, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 10, 10, 10]
@@ -18,6 +28,8 @@ house_deck.append(random.choice(card_list))
 player_bust = False
 stand = False
 house_bust = False
+
+#Writes the json save file with newer variables 
 def jason_derulo():
     global money
     global wins
@@ -25,12 +37,15 @@ def jason_derulo():
     jason_dump = {
         'Money': money,
         'Wins': wins,
-        'Losses': losses
+        'Losses': losses,
+        'Bankrupts': bankrupts,
+        "Debt": debt
     }
     with open('jason_derulo.json', 'w') as derulo_dump:
         json.dump(jason_dump, derulo_dump, indent = 4, sort_keys = True)
 jason_derulo()
 
+#Draws a random card to put into the deck. Uses which_deck to decide which deck to draw to.
 def draw(which_deck):
     global ace
     global player_deck
@@ -41,7 +56,8 @@ def draw(which_deck):
     else:
         ace = 11
     which_deck.append(picked_card)
- 
+
+ #Has all the game over scenarios and adds losses or wins.
 def game_over():
     global game
     global wins
@@ -86,6 +102,7 @@ def game_over():
         print("Losses: ", losses, "Wins ", wins)
         return
 
+#Checks for busts
 def bust_check():
     global game
     if game == True:
@@ -109,14 +126,28 @@ while running == True:
     draw(player_deck)
     draw(house_deck)
     draw(house_deck)
-    print(money)
+    #To check if you are bankrupt and if so makes you in debt.
+    if money == 0 or money < 0:
+        money = 20
+        time.sleep(1)
+        print("You are bankrupt. You get $20 but you owe me...")
+        debt += 20
+        bankrupts += 1
+        jason_derulo()
+        time.sleep(1)
+    #Asks if you want to pay back your debt.
+    if money > debt and debt > 0:
+        time.sleep(1)
+        debt_collection = input("Do you want to pay back your debt? yes/no >>")
+        if debt_collection == "yes":
+            money -= debt
+    time.sleep(1)
+    #Asks for how much money you want to bet.
+    print("your money: ", money)
     bet = input("How much money do you want to bet? >> ")
     bet = int(bet)
     while bet > money or bet < 0:
         bet = int(input("No. Try again >> "))
-    if money == 0 or money < 0:
-        quit()
-    
     print("Player deck", sum(player_deck))
     print("House card", house_deck[1])
     while game == True:
